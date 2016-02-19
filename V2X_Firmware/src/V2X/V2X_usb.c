b4f5689917dcd71828454e895f3b26ddadf36aff
Jesse Banks <jbanks2@jaguarlandrover.com>
2016-02-19 12:43:46 -0800

    3 USB ports with pass through to STN
    functioning configurable USB port through to the STN. can talk and
    change stn baud and follow with terminal window.

--- V2X_Firmware/src/V2X/V2X_usb.c
+++ V2X_Firmware/src/V2X/V2X_usb.c
@@ -6,10 +6,8 @@
  */ 
 
 #include "V2X.h"
-#include "conf_usb.h"
-#include "usb_protocol_cdc.h"
 
-static volatile bool usb_b_cdc_enable = false;
+static volatile bool usb_cdc_enabled_bool[3] = {false, false, false};
 
 void usb_suspend_action(void)
 {
@@ -23,22 +21,25 @@ void usb_resume_action(void)
 
 bool usb_cdc_enable(uint8_t port)
 {
-	usb_b_cdc_enable = true;
+	usb_cdc_enabled_bool[port] = true;
 	uart_open(port);	// Open communication
 	return true;
 }
 
 void usb_cdc_disable(uint8_t port)
 {
-	usb_b_cdc_enable = false;
+	usb_cdc_enabled_bool[port] = false;
 	// Close communication
 	uart_close(port);
 }
 
 void usb_sof_action(void)  //causes led 0 to flash on USB activity
 {
-	if (!usb_b_cdc_enable)
-	return;
+	if (!usb_cdc_enabled_bool[0] | !usb_cdc_enabled_bool[1] | !usb_cdc_enabled_bool[2]) {
+		led_0_off(); 
+		return;
+	}
+		
 	int framenumber = udd_get_frame_number();
 	if (0 == framenumber) {
 		led_0_on();
@@ -53,6 +54,7 @@ void usb_cdc_set_dtr(uint8_t port, bool b_enable)
 	if (b_enable) {
 		// Host terminal has open COM
 		//possibly enable ELM or ACL
+		//change can from direct to pass through mode
 		}else{
 		// Host terminal has close COM
 	}
