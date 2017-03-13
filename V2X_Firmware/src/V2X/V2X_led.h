28eb6c9cc1d9f1ac88b0e1149b90e7a7cf0d149f
Steve Miller <smiller6@jaguarlandrover.com>
2017-03-13 10:18:39 -0700

    Remap LED ports
    Leds are mapped to new output compare ports (PORTE, 1, 2, 3).
    Though still driven completely by software, future revisions should utilize the Timer0 peripheral to drive the port activity.

--- V2X_Firmware/src/V2X/V2X_led.h
+++ V2X_Firmware/src/V2X/V2X_led.h
@@ -9,6 +9,17 @@
 #ifndef V2X_LED_H_
 #define V2X_LED_H_
 
+#if V2X_REV >= REV_20
+/* Revision 2.0:
+ * LED pins have been remapped from the ADC (analog-digital) channel ports to 
+ * OC (output compare). Currently, the pins are driven completely in software, 
+ * on update, which is undesireable and inefficient.
+ * In the future, the LEDs should be driven by using PWM from Timer0, so that 
+ * updates are sent only when necessary, and the LEDs are driven by hardware
+ * peripheral.
+ */
+#endif
+
 /**
  * @def charge_pump_toggle
  * @brief Causes the pin driving the charge pump circuit to toggle state HIGH<>LOW
