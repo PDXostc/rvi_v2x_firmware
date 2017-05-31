f28aaa0c430dbc7a65e09a8bcafa03b5be318be0
Steve Miller <smiller6@jaguarlandrover.com>
2017-05-31 15:44:56 -0700

    Eliminate board compatability for power

--- V2X_Firmware/src/V2X/V2X_power.c
+++ V2X_Firmware/src/V2X/V2X_power.c
@@ -43,21 +43,10 @@ void PWR_clear(void)
 }
 
 void PWR_push(void) {  //uses shift_register_state to update the shift register
-#if V2X_REV <= REV_12
-/* With 2 shift registers, it is necessary to copy the power control values into
- * 2 packets, with MSB written first, so that the 2 byte sequence lands on the
- * register outputs in the proper order.
- */
-	uint8_t data[2];
-	data[1] = power_control_state & 0xff;
-	data[0] = (power_control_state >> 8) & 0xff;
-	spi_write_packet(SR_SPI, data, 2);
-#elif V2X_REV >= REV_20
 	/* We should be able to send the single byte to the single shift register,
 	 * as long as the bit order has been respected.
 	 */
 	spi_write_packet(SR_SPI, &power_control_state, 1);
-#endif
 	PWR_latch();
 }
 
@@ -74,19 +63,6 @@ Bool PWR_query(SHIFT_REGISTER_TYPE mask) {
 	else {return false;}
 }
 
-#if V2X_REV <= REV_12
-void PWR_hub_start(void) {
-	PWR_turn_on((1<<ENABLE_HUB));
-	PWR_push();
-}
-
-void PWR_hub_stop(void){
-	PWR_turn_off((1<<ENABLE_HUB));
-	PWR_push();
-}
-#endif
-
-#if V2X_REV >= REV_20
 /* 3 volt power pin manipulation
  * Use with caution! 3v should only be enabled when not using 4v
  * When transitioning to 3v power only, enable 3v immediately before disabling
@@ -144,8 +120,6 @@ void PWR_shutdown(void) {
 	PWR_3_stop();
 }
 
-#endif
-
 void PWR_host_start(void) {
 	PWR_turn_on((1<<ENABLE_5V0B));
 	PWR_is_5_needed();
@@ -182,34 +156,14 @@ void PWR_can_start (void) {
 };
 
 void PWR_gsm_stop(void) {
-#if V2X_REV <= REV_12
-	GSM_command_power_off();
-	PWR_turn_on(1<<ENABLE_SIM_PWR_ON);
-#elif V2X_REV >= REV_20
 	// GSM_command_power_off();
 	PWR_turn_on(1<<ENABLE_SIM_PWR_ON);
-#endif
 	PWR_push();
 }
 
 void PWR_gsm_start(void) {
-#if V2X_REV <= REV_12
-	//FIXME: Trying one extra measure to get the sim to respond using only the power pin...
-	PWR_turn_off(1<<ENABLE_SIM_RESET);
-	PWR_push();
-	delay_ms(500);
-	PWR_turn_on((1<<ENABLE_SIM_RESET)|(1<<ENABLE_4V1));  //release the reset
-	PWR_push();
-	if (sim_power_status())
-	{
-		PWR_turn_off((1<<ENABLE_SIM_PWR_ON));
-		PWR_push();
-		delay_ms(500);
-	}
-#elif V2X_REV >= REV_20
 	PWR_turn_on(1<<ENABLE_SIM_RESET);
 	PWR_4_start();
-#endif
  	if (PWR_query(1<<ENABLE_SIM_PWR_ON))
 	{
 		PWR_turn_off((1<<ENABLE_SIM_PWR_ON));
