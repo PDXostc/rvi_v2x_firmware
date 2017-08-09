3e2c77b89d12b0b8b6b997592c883403d16c85af
lillialexis <lilli@daoofdev.com>
2017-08-09 13:32:01 -0700

    [VX-16] Added is_low_power check. Updated description of what the car off/on state check method will need to do.

--- V2X_Firmware/src/V2X/V2X_power.c
+++ V2X_Firmware/src/V2X/V2X_power.c
@@ -8,6 +8,8 @@
 
 #include "V2X.h"
 
+Bool isLowPower = true;
+
 void PWR_init(void)
 {
 	/* Need to drive high 3v3_EN signal here, to ensure CPU operation.
@@ -58,23 +60,65 @@ Bool PWR_query(SHIFT_REGISTER_TYPE mask) {
 	else {return false;}
 }
 
-void PWR_car_on_state_check() {
-/*
-	This function must:
-	Change power state to enable CAN device
-	Configure can device
-	Take measurement of battery voltage
-	Decide what to do:
+Bool PWR_is_low_power() {
+    return (Bool) ((power_control_state & ((1<<ENABLE_4V1) | (1<<ENABLE_5V0) | (1<<ENABLE_5V0B))) == 0);
+}
 
-	- less than 11V do no reschedule checking job
-	- less than 13V and more than 11V reschedule checking job
-	- more than 13V needs follow up
-		-- configure can setup
-		-- check for RPM
-		-- start Rpi bring up sequence if greater than 0
-*/
-	usb_tx_string_PV(PSTR("Car-on sleep-state check!\n"));
-	job_set_timeout(SYS_CAR_ON_STATE_CHECK, 3);
+void PWR_car_on_state_check() {
+
+
+    usb_tx_string_PV(PSTR("Car-on sleep-state check!\n"));
+
+    if (PWR_is_low_power()) {
+
+
+
+        job_set_timeout(SYS_CAR_ON_STATE_CHECK, 3);
+
+    } else {
+
+
+
+        job_set_timeout(SYS_CAR_ON_STATE_CHECK, 3);
+
+    }
+
+	/*
+     *
+     * first check if low power. if not low power, check if car is still running by listening to the CAN bus.
+     *
+     * if low power, do the can stuff. this will pull us out of low power, so if the check puts us back into the 'do
+     * nothing' state, we need to make sure to go back to low power so that our isLowPower check is reset.
+     */
+
+
+    /*
+        This function must:
+        Check if low power or high power
+
+        If low power:
+            Change power state to enable CAN device (because we're in low power mode)
+                (turn on the 4 and the 5 and it will give me the 3, maybe? - probably happens automatically)
+
+            Configure can device
+                ()
+
+            Take measurement of battery voltage (to see if we need to kill ourselves completely?)
+                ()
+
+            Decide what to do:
+                - less than 11V do no reschedule checking job, power off
+                - less than 13V and more than 11V reschedule checking job
+                - more than 13V needs follow up
+                    -- configure can setup (wouldn't this already be done?)
+                    -- check for RPM (scratch that... just listen for CAN noise)
+                    -- start raspi bring up sequence if greater than 0 (keep the job, though)
+
+        If high power:
+            Listen for CAN noise
+                - If silent, kill the raspi, reschedule job (at longer interval)
+                - If not silent, reschedule job (at shorter interval)
+    */
 }
 
 /* 3 volt power pin manipulation
